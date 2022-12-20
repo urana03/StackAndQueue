43ca9183f8e5353922f55e5f4678721b1f869b04
urana03 <uhackninja@gmail.com>
2022-12-20 20:22:34 +0530

    Add:Enqueue the elements in the queue using linked list

---
+++ src/main/java/com/bridgelabz/Queue.java
@@ -0,0 +1,54 @@
+package com.bridgelabz;
+
+class Node{
+   
+   int data;
+   Node next;
+   
+   Node(int data){
+       
+       this.data = data;
+       this.next =  null;
+   }
+   
+}
+public class Queue{
+    static Node head;
+public void enqueue(int data) {
+   
+   Node newNode = new Node(data);
+   
+   if(head == null) {
+       head = new Node(data);
+       return;
+   }
+   newNode.next = null;
+   
+   Node last = head;
+   while(last.next != null)
+       last = last.next;
+   last.next = newNode;
+   return;
+}
+
+public void printList() {
+   
+   Node n = head;
+   while(n != null) {
+       System.out.print(n.data+" ");
+       n=n.next;
+   }
+}
+
+public static void main(String args[]) {
+
+   Queue obj = new Queue();
+   
+   obj.enqueue(56);
+   obj.enqueue(30);
+   obj.enqueue(70);
+   
+   obj.printList();
+}
+}
+
